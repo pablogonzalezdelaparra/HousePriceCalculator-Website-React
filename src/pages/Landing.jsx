f3efdaa3b165036232dc3fe0c79ba719402a5591
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-08-31 16:09:06 -0600

    Cambio estéticos a Landing y Navbar

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,12 +5,12 @@ function Landing() {
   return (
     <div className='landing-main-container'>
         <div className='main-text'>
-            <h1 className='title'>Tu propiedad,<br/>tus reglas</h1>
+            <h1 className='title'>Tu propiedad,<br/>tus reglas.</h1>
             <p className='description'>Descubre al instante el valor de cualquier propiedad con nuestra plataforma de cálculo inmobiliario. </p>
         </div>
         <div className='second-text'>
-            <h2 className='title'><span className='first-title'>Precios justos</span><br/><span className='second-title'>Sin complicaciones</span></h2>
-            <button className='call-to-action'>Descubre cómo</button>
+            <h2 className='title'><span className='first-title'>Precios justos.</span><br/><span className='second-title'>Sin complicaciones.</span></h2>
+            <a href='#' className='call-to-action'>Descubre cómo</a>
         </div>
     </div>
   )
