f51ac508aed6cba975db5efaed6e26122b1b7112
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-09-01 18:17:00 -0600

    Create commentService.js

---
+++ src/services/commentService.js
@@ -0,0 +1,34 @@
+import { API_BASE_URL } from "../config";
+
+export async function getComments() {
+  const validateComments = await fetch(`${API_BASE_URL}/comment/getComments`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(async (response) => {
+    const data = await response.json();
+    return data.commentObjects;
+  });
+  return validateComments;
+}
+
+export async function createComment(commentInfo) {
+  const authToken = Cookies.get("userToken");
+
+  const bodyCommentData = {
+    comment: commentInfo,
+  };
+  const validateComment = await fetch(`${API_BASE_URL}/comment/createComment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${authToken}`,
+    },
+    body: JSON.stringify(bodyCommentData),
+  }).then(async (response) => {
+    const data = await response.json();
+    return data.message;
+  });
+  return validateComment;
+}
