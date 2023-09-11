6fe5e0de0ee89d6932c9d854e565c42317d55aec
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-09-11 17:32:22 -0600

    Crear los Tweets

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import Comentarios from '../components/Comentarios';
 import DashboardsC from '../components/DashboardsC';
 import FormCotizacion from '../components/FormCotizacion';
 import Footer from '../components/Footer';
+import TweetObj from '../components/Tweet';
 
 function HomePage() {
   return (
@@ -15,6 +16,7 @@ function HomePage() {
         <FormCotizacion />
         <DashboardsC />
         <Comentarios />
+        <TweetObj />
         <Footer />
     </div>
   )
