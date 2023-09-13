1c98bef8adb45353220a06ab0634f1ec2278f6e1
Pablo González de la Parra <pablo.gonzalezdlp@gmail.com>
2023-09-12 21:28:34 -0600

    Envío de información a modelo en Flask

--- src/services/modelService.js
+++ src/services/modelService.js
@@ -1,45 +1,48 @@
 import { FLASK_BASE_URL } from "../config";
 
-export async function getPrediction() {
-
-    bodyParameterData = {
-        LotArea: "2345",
-        TotalBsmtSF: "2345",
-        YearBuilt: "12345",
-        GrLivArea: "2345",
-        BsmtFullBath: "2345",
-        HalfBath: "2345",
-        KitchenAbvGr: "23456",
-        Fireplaces: "23456",
-        GarageCars: "23456",
-        ScreenPorch: "23456",
-        WoodDeckSF: "2345",
-        BsmtExposure: "Sin exposición",
-        MSZoning: "MSZoning_RL",
-        Neighborhood: "Neighborhood_NridgHt",
-        Condition1: "Condition2_PosA",
-        BldgType: "-1",
-        Exterior1st: "Exterior1st_MetalSd",
-        Foundation: "Foundation_Wood",
-        Heating: "Foundation_Wood",
-        Functional: "Functional_Min2",
-        SaleType: "SaleType_WD",
-        RoofMatl: "-1",
-        CentralAir: "1",
-        LotConfig_Inside: "1",
-        Exterior2nd_BrkFace: "1",
-        LandSlope_Sev: "0",
-        Electrical_SBrkr: "0",
-        GarageType_2Types: "1",
-        SaleCondition_Normal: "1"
+export async function getPrediction(parameterInfo) {
+  const bodyParameterData = {
+    LotArea: parameterInfo.LotArea,
+    YearBuilt: parameterInfo.YearBuilt,
+    BsmtExposure: parameterInfo.BsmtExposure,
+    BsmtFinSF1: parameterInfo.BsmtFinSF1,
+    TotalBsmtSF: parameterInfo.TotalBsmtSF,
+    CentralAir: parameterInfo.CentralAir,
+    GrLivArea: parameterInfo.GrLivArea,
+    BsmtFullBath: parameterInfo.BsmtFullBath,
+    HalfBath: parameterInfo.HalfBath,
+    KitchenAbvGr: parameterInfo.KitchenAbvGr,
+    Fireplaces: parameterInfo.Fireplaces,
+    GarageCars: parameterInfo.GarageCars,
+    WoodDeckSF: parameterInfo.WoodDeckSF,
+    ScreenPorch: parameterInfo.ScreenPorch,
+    MSZoning: parameterInfo.MSZoning,
+    LotConfig_Inside: parameterInfo.LotConfig_Inside,
+    LandSlope_Sev: parameterInfo.LandSlope_Sev,
+    Neighborhood: parameterInfo.Neighborhood,
+    Condition1: parameterInfo.Condition1,
+    BldgType: parameterInfo.BldgType,
+    RoofMatl: parameterInfo.RoofMatl,
+    Exterior1st: parameterInfo.Exterior1st,
+    Exterior2nd_BrkFace: parameterInfo.Exterior2nd_BrkFace,
+    Foundation: parameterInfo.Foundation,
+    Heating: parameterInfo.Heating,
+    Electrical_SBrkr: parameterInfo.Electrical_SBrkr,
+    Functional: parameterInfo.Functional,
+    GarageType_2Types: parameterInfo.GarageType_2Types,
+    SaleType: parameterInfo.SaleType,
+    SaleCondition_Normal: parameterInfo.SaleCondition_Normal,
+  };
+  const validatePrediction = await fetch(
+    `${FLASK_BASE_URL}/model/getPrediction`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyParameterData),
     }
-  const validatePrediction = await fetch(`${FLASK_BASE_URL}/model/getPrediction`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(parameterBodyData),
-  }).then(async (response) => {
+  ).then(async (response) => {
     const data = await response.json();
     return data;
   });
